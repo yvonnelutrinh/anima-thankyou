dca681759b440e53491b1bb720f24979bc6025cd
yvonnelutrinh <yvonnelutrinh@gmail.com>
2025-11-02 13:11:53 -0500

    add react hook form placeholder

--- src/components/ThankYouForm/index.tsx
+++ src/components/ThankYouForm/index.tsx
@@ -1,13 +1,24 @@
+'use client';
+import { useForm } from "react-hook-form";
+
 export default function ThankYouForm() {
+  const { register, handleSubmit, watch } = useForm();
+
+  //TODO: delete console log + convert to PDF download
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("firstName"));
+  console.log(watch("lastName"));
+
   return (
-    <form action="">
+    <form onSubmit={handleSubmit(onSubmit)}>
       <label>
         First name:
-        <input type="text" />
+        <input type="text" {...register("firstName")} />
       </label>
       <label>
         Last name:
-        <input type="text" />
+        <input type="text" {...register("lastName")} />
       </label>
       <button type="submit">Download PDF</button>
     </form>
