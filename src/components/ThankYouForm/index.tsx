9f029b5728e22e6b2830bc581d6a0bd4db7c29f4
yvonnelutrinh <yvonnelutrinh@gmail.com>
2025-11-02 15:43:13 -0500

    add custom message to PDF with bounding box

--- src/components/ThankYouForm/index.tsx
+++ src/components/ThankYouForm/index.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { useForm } from "react-hook-form";
 import PDFPreview from "../PDFPreview";
-import { PDFDocument, rgb } from "pdf-lib";
+import { PDFDocument, rgb, layoutMultilineText, TextAlignment } from "pdf-lib";
 import fontkit from "@pdf-lib/fontkit";
 
 const IMAGEDIMENSIONS = { width: 754, height: 387 };
 const PDFTEXT = "Thank you for supporting us";
 
 export default function ThankYouForm() {
+  // custom message generator
   function customPDFText(firstName, lastName, baseText) {
     if (firstName || lastName) {
       return baseText + ", " + firstName + " " + lastName + "!";
     }
     return baseText + "!";
   }
+
   // instantiate form, controlled inputs, handle submit
   const { register, handleSubmit, watch } = useForm();
   const onSubmit = (data) => generatePDF(data);
@@ -31,27 +33,50 @@ export default function ThankYouForm() {
     );
     const poppins = await pdfDoc.embedFont(fontBytes);
 
-    // add blank page to the document with image dimensions
-    const page = pdfDoc.addPage([
-      IMAGEDIMENSIONS.width,
-      IMAGEDIMENSIONS.height,
-    ]);
+    // add image to page
+    const pngImageBytes = await fetch("/assets/arctic-fox.png").then((res) =>
+      res.arrayBuffer()
+    );
 
-    // get the width and height of the page
-    const { width, height } = page.getSize();
+    const pngImage = await pdfDoc.embedPng(pngImageBytes);
 
-    // draw string of text near top of the page
+    // add blank page to the document using image dimensions
+    const page = pdfDoc.addPage([pngImage.width, pngImage.height]);
+
+    // draw image onto page
+    page.drawImage(pngImage, {
+      width: pngImage.width,
+      height: pngImage.height,
+    });
+
     const fontSize = 30;
-    page.drawText(
+
+    // define boundaries for text
+    const layout = layoutMultilineText(
       customPDFText(watch("firstName"), watch("lastName"), PDFTEXT),
       {
-        x: 50,
-        y: height - 4 * fontSize,
-        size: fontSize,
+        bounds: {
+          x: 50,
+          y: pngImage.height - 6 * fontSize,
+          width: pngImage.width * 0.6, // text width is 60% of image
+          height: pngImage.height,
+        },
         font: poppins,
-        color: rgb(0, 0, 0),
+        fontSize: fontSize,
+        alignment: TextAlignment.Left,
       }
     );
+
+    // draw string of text near top of the page
+    layout.lines.forEach((line, i) => {
+      page.drawText(line.text, {
+        x: layout.bounds.x,
+        y: layout.bounds.y - i * layout.lineHeight,
+        // size: layout.fontSize,
+        // font: layout.font,
+        color: rgb(1, 1, 1),
+      });
+    });
 
     // serialize pdf to bytes
     const pdfBytes = await pdfDoc.save();
