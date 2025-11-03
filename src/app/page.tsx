36633e2f57ed616c46e1d4c1b452468a32a38f9e
yvonnelutrinh <yvonnelutrinh@gmail.com>
2025-11-02 20:56:42 -0500

    update text preview scaling, content margins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import CustomPDFGenerator from "../components/CustomPDFGenerator";
 
 export default function Home() {
   return (
-    <main className="max-w-3xl m-16">
+    <div className="my-16 mx-8">
+    <main className="max-w-3xl mx-auto
+">
       <h1 className="px-4 size-fit
  rounded-xl ring-1 ring-cyan-500 text-lg tracking-wide text-[var(--accent-teal)] uppercase">
         Thank you{" "}
@@ -10,5 +12,6 @@ export default function Home() {
       <h2 className="mt-6 text-3xl sm:text-4xl md:text-5xl">Your support is appreciated</h2>
       <CustomPDFGenerator />
     </main>
+    </div>
   );
 }
--- src/components/PDFPreview/index.tsx
+++ src/components/PDFPreview/index.tsx
@@ -8,17 +8,16 @@ interface PDFPreviewProps {
 
 export default function PDFPreview({ width, height, pdfText }: PDFPreviewProps) {
   return (
-    <div
-      className="relative inline-block" id="pdf-preview">
-      <p
-        className="absolute text-white left-12 bottom-[30%] w-2/5 
-        text-[clamp(0.5rem,4vw,2.5rem)]"
-      >
-        {pdfText}
-      </p>
+    <div className="relative inline-block overflow-hidden" id="pdf-preview">
+      <div className="absolute inset-0 flex items-center">
+        <p 
+          className="text-white ml-12 w-2/5 text-[clamp(0.5rem,4vw,2.5rem)]"
+        >
+          {pdfText}
+        </p>
+      </div>
       <Image
         src="/assets/arctic-fox.png"
-        className="rounded-sm"
         alt="Portrait of an Arctic fox looking at the camera"
         width={width}
         height={height}
